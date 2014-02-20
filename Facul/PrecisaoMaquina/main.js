d1286ad23e7073a7dc13c34d33a7bc2ee5cfeb41
George Jegon <george_jegon@hotmail.com>
2014-02-20 01:25:18 -0300

    Adicionando funcao e^x e fazendo algumas verificoes pedidas

--- Facul/PrecisaoMaquina/main.js
+++ Facul/PrecisaoMaquina/main.js
@@ -8,9 +8,32 @@ function precisaoMaquina(r){
 	return 2*a;
 }//END FUNCTION
 
+function precisaoMaquinaArray(a){
+	if(isArray(a)){
+		a.forEach(function(o){
+			console.log(precisaoMaquina(o));
+		});
+	}//END IF
+	
+}//END FUNCTION
+
+function isArray(a){
+	return (a instanceof Array);
+}//END FUNCTION
+
 function fat(n){
 	if(n<=0){
 		return 1;
 	}//END IF
 	return n * fat(n-1);
 }//END FUNCTION
+
+function taylorEulerExp(v,n){
+	if(n<=1){
+		return 1;
+	}//END IF
+	return taylorEulerExp(v,n-1)+(Math.pow(v,n-1)/fat(n-1));
+}//END FUNCTION
+
+//precisaoMaquinaArray([10,17,100,184,1000,1575,10000,17893]);
+taylorEulerExp(5,28);
