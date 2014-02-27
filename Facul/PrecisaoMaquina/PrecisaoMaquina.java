c8dd9b5b25624ae9204ed44e146c88eabdaa4930
Geeh Jegon <george_jegon@hotmail.com>
2014-02-27 17:19:34 -0300

    Adicionando Precisão Dupla

--- Facul/PrecisaoMaquina/PrecisaoMaquina.java
+++ Facul/PrecisaoMaquina/PrecisaoMaquina.java
@@ -6,6 +6,7 @@ public class PresicaoMaquina {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(precisaoMaquina());
+		System.out.println(precisaoMaquinaDouble());
 	}//END FUNCTION
 	
 	
@@ -24,6 +25,23 @@ public class PresicaoMaquina {
 	
 	public static float precisaoMaquina(int[] r){
 	   return 0.0f;
+	}//END FUNCTION
+	
+	public static double precisaoMaquinaDouble(){
+		return precisaoMaquina(1);
+	}//END FUNCTION
+	
+	public static double precisaoMaquinaDouble(int r){
+	    double a = 1, s = (a + r);
+	    while(s>r){
+	    	a /= 2;
+	    	s = r + a;
+	    }//END WHILE
+		return 2*a;
+	}//END FUNCTION
+	
+	public static double precisaoMaquinaDouble(int[] r){
+	   return 0.0d;
 	}//END FUNCTION
 	
 }//END CLASS
