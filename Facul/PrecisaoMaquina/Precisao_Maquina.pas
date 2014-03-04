51d40ca29f5021b785b71d73b30e487345bc4473
Geeh Jegon <george_jegon@hotmail.com>
2014-03-04 00:11:19 -0300

    Update Precisao_Maquina.pas

--- Facul/PrecisaoMaquina/Precisao_Maquina.pas
+++ Facul/PrecisaoMaquina/Precisao_Maquina.pas
@@ -28,7 +28,31 @@ begin
     precisaoMaquinaDupla := 2*a;
 end;
 
+procedure precisaoMaquinaArray (r: array of integer); 
+var 
+    i,t: integer;
+begin 
+    t := Length(r)-1;
+    for i:=0 to t do
+        begin
+            writeln('Valor de referência: ',r[i]);
+            writeln('Precisão Dupla da Maquina: ', precisaoMaquinaSimples(r[i]));
+            writeln('Precisão Dupla da Maquina: ', precisaoMaquinaDupla(r[i]));
+            writeln('--------------------------------------------------------');
+        end;
+end;
+
+var 
+	ArrayRefs: array[0..8] of integer;
 begin
-    writeln(precisaoMaquinaSimples(1)); 
-    writeln(precisaoMaquinaDupla(1)); 
+    ArrayRefs[0] := 1;
+	ArrayRefs[1] := 10;
+	ArrayRefs[2] := 17;
+	ArrayRefs[3] := 100;
+	ArrayRefs[4] := 184;
+	ArrayRefs[5] := 1000;
+	ArrayRefs[6] := 1575;
+	ArrayRefs[7] := 10000;
+	ArrayRefs[8] := 17893;
+    precisaoMaquinaArray(ArrayRefs);
 end.  
