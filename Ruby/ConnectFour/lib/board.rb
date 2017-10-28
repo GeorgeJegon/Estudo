12f3544608dc40f44f078d97095e6a4cc08959ff
George Jegon <george_jegon@hotmail.com>
2017-10-28 03:36:19 -0200

    Defines method to drop one piece at board grid column

--- Ruby/ConnectFour/lib/board.rb
+++ Ruby/ConnectFour/lib/board.rb
@@ -6,6 +6,20 @@ class Board
   def initialize
     @grid   = Array.new(LINES){ Array.new(COLUMNS) { 0 } }
     @errors = []
+  end
+
+  def drop(column, symbol)
+    if valid_column?(column)
+      line = line_playable_column(column)
+      if line
+        line[column] = symbol
+      else
+        @errors << "Todas as linhas desta coluna já foram preenchidas"
+      end
+    else
+      @errors << "Número de Coluna inválido!"
+    end
+  end
   def draw
     puts @grid.reverse.map(&method(:draw_line)).join("\n")
   end
@@ -16,6 +30,17 @@ class Board
   def set_grid(grid)
     @grid = grid.reverse
   end
+
+  private
+
+  def valid_column?(column)
+    column.between?(0, COLUMNS)
+  end
+
+  def line_playable_column(column)
+    @grid.detect { |line| line[column].zero? }
+  end
+
   def draw_column(column)
     column.zero? ? " " : column
   end
