4dcf8e8751c326374c3c45cee3ece13f71c2b496
George Jegon <george_jegon@hotmail.com>
2017-10-28 03:37:48 -0200

    Defines method and attribute to control next turns

--- Ruby/ConnectFour/lib/game.rb
+++ Ruby/ConnectFour/lib/game.rb
@@ -7,10 +7,18 @@ class Game
   def_delegators :@board, :board_string
 
   def initialize
+    @currentTurn = 0
     @board = Board.new
     draw_board
   def draw_board
     @board.draw
+  end
+  protected
+
+  def next_turn
+    draw_board
+    @currentTurn += 1
+    @current_player = (@currentTurn % 2) + 1
   end
   end
 end
