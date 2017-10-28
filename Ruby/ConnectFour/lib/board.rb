f8a0e8ef08fbb4c689b2cbd4db428bdbc375f60d
George Jegon <george_jegon@hotmail.com>
2017-10-28 03:31:27 -0200

    Defining draw board method

--- Ruby/ConnectFour/lib/board.rb
+++ Ruby/ConnectFour/lib/board.rb
@@ -3,5 +3,14 @@ class Board
   LINES = 6
   def initialize
     @grid   = Array.new(LINES){ Array.new(COLUMNS) { 0 } }
+  def draw
+    puts @grid.reverse.map(&method(:draw_line)).join("\n")
+  end
+  def draw_column(column)
+    column.zero? ? " " : column
+  end
+
+  def draw_line(line)
+    line.map(&method(:draw_column)).join("|").prepend('|').concat('|')
   end
 end
