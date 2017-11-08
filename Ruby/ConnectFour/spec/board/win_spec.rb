3d9ff8bacd0b62388f74bd9165c2f906c5bde892
George Jegon <george_jegon@hotmail.com>
2017-11-08 00:25:37 -0200

    Extract board constants spec to own file

--- Ruby/ConnectFour/spec/board/win_spec.rb
+++ Ruby/ConnectFour/spec/board/win_spec.rb
@@ -1,43 +1,79 @@
-describe Board do
-  describe "Constants" do
-
-    context "columns" do
-      it "returns 7" do
-        expect(described_class.const_get(:COLUMNS)).to eq 7
-      end
-    end
-
-    context "lines" do
-      it "returns 6" do
-        expect(described_class.const_get(:LINES)).to eq 6
-      end
+describe Board, "#win?" do
+  context "when board's grid is empty" do
+    it "returns false" do
+      expect(subject.win?(1)).to be false
     end
   end
 
-  describe "#win?" do
-    context "when board's grid is empty" do
-      it "returns false" do
-        expect(subject.win?(1)).to be false
-      end
+  context "when board's grid has one piece" do
+    before { subject.drop(1, 1) }
+
+    it "returns false" do
+      expect(subject.win?(1)).to be false
+    end
+  end
+
+  context "when board's grid has horizontal winner" do
+    before do
+      subject.set_grid([
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,1,1,1,1,0,0]
+      ])
     end
 
-    context "when board's grid has one piece" do
-      before { subject.drop(1, 1) }
+    it "returns true" do
+      expect(subject.win?(1)).to be true
+    end
+  end
 
-      it "returns false" do
-        expect(subject.win?(1)).to be false
-      end
+  context "when board's grid has four player symbol in sequence but not in a row" do
+    before do
+      subject.set_grid([
+        [0,0,0,0,0,1,1],
+        [1,1,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0]
+      ])
     end
 
-    context "when board's grid has horizontal winner" do
+    it "returns false" do
+      expect(subject.win?(1)).to be false
+    end
+  end
+
+  context "when board's grid has vertical winner" do
+    before do
+      subject.set_grid([
+        [0,0,0,0,0,0,0],
+        [0,0,0,1,0,0,0],
+        [0,0,0,1,0,0,0],
+        [0,0,0,1,0,0,0],
+        [0,0,0,1,0,0,0],
+        [0,0,0,0,0,0,0]
+      ])
+    end
+
+    it "returns true" do
+      expect(subject.win?(1)).to be true
+    end
+  end
+
+  context "when board's grid has diagonal winner" do
+    context "and it is primary" do
       before do
         subject.set_grid([
+          [1,0,0,0,0,0,0],
+          [0,1,0,0,0,0,0],
+          [0,0,1,0,0,0,0],
+          [0,0,0,1,0,0,0],
           [0,0,0,0,0,0,0],
-          [0,0,0,0,0,0,0],
-          [0,0,0,0,0,0,0],
-          [0,0,0,0,0,0,0],
-          [0,0,0,0,0,0,0],
-          [0,1,1,1,1,0,0]
+          [0,0,0,0,0,0,0]
         ])
       end
 
@@ -46,32 +82,15 @@ describe Board do
       end
     end
 
-    context "when board's grid has four player symbol in sequence but not in a row" do
+    context "and it is primary 2" do
       before do
         subject.set_grid([
-          [0,0,0,0,0,1,1],
-          [1,1,0,0,0,0,0],
-          [0,0,0,0,0,0,0],
-          [0,0,0,0,0,0,0],
-          [0,0,0,0,0,0,0],
-          [0,0,0,0,0,0,0]
-        ])
-      end
-
-      it "returns false" do
-        expect(subject.win?(1)).to be false
-      end
-    end
-
-    context "when board's grid has vertical winner" do
-      before do
-        subject.set_grid([
-          [0,0,0,0,0,0,0],
+          [1,0,0,0,0,0,0],
+          [0,1,0,0,0,0,0],
+          [0,0,1,0,0,0,0],
           [0,0,0,1,0,0,0],
-          [0,0,0,1,0,0,0],
-          [0,0,0,1,0,0,0],
-          [0,0,0,1,0,0,0],
-          [0,0,0,0,0,0,0]
+          [0,0,0,0,1,0,0],
+          [0,0,0,0,0,1,0]
         ])
       end
 
@@ -80,39 +99,20 @@ describe Board do
       end
     end
 
-    context "when board's grid has diagonal winner" do
-      context "and it is primary" do
-        before do
-          subject.set_grid([
-            [1,0,0,0,0,0,0],
-            [0,1,0,0,0,0,0],
-            [0,0,1,0,0,0,0],
-            [0,0,0,1,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0]
-          ])
-        end
-
-        it "returns true" do
-          expect(subject.win?(1)).to be true
-        end
+    context "and it is secondary" do
+      before do
+        subject.set_grid([
+          [0,0,0,0,0,0,0],
+          [0,0,0,1,0,0,0],
+          [0,0,1,0,0,0,0],
+          [0,1,0,0,0,0,0],
+          [1,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0]
+        ])
       end
 
-      context "and it is secondary" do
-        before do
-          subject.set_grid([
-            [0,0,0,0,0,0,0],
-            [0,0,0,1,0,0,0],
-            [0,0,1,0,0,0,0],
-            [0,1,0,0,0,0,0],
-            [1,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0]
-          ])
-        end
-
-        it "returns true" do
-          expect(subject.win?(1)).to be true
-        end
+      it "returns true" do
+        expect(subject.win?(1)).to be true
       end
     end
   end
