58947dab8747138e1f89f19ce0372194c29f7161
George Jegon <george_jegon@hotmail.com>
2017-11-08 02:52:17 -0200

    Added Horizontal Traversal Strategy to board checker

--- Ruby/ConnectFour/lib/board_checker.rb
+++ Ruby/ConnectFour/lib/board_checker.rb
@@ -2,3 +2,4 @@ module BoardChecker; end
 
 require "board_checker/base"
 require "board_checker/by_regex"
+require "board_checker/by_traversal"
---
+++ Ruby/ConnectFour/lib/board_checker/by_traversal.rb
@@ -0,0 +1,5 @@
+module BoardChecker
+  module ByTraversal; end
+
+  require "board_checker/by_traversal/horizontal"
+end
